7b55b6df8c23aca2d0bdc4663a48c9b433a35b89
Andrew <goldsloam@gmail.com>
2018-03-26 16:29:15 -0700

    Update ricing-suckless.sh
    stupid spacing issues...

--- ricing-suckless.sh
+++ ricing-suckless.sh
@@ -20,8 +20,8 @@
 # wireless_tools  : wpa_supplicant and the like
 # networkmanager  : trust me... you don't want to do wireless yourself
 # xf86-video-intel: intel video drivers
-# vim		  	  : more than vi
-# wget			  : when curl doesn't cut it
+# vim                : more than vi
+# wget              : when curl doesn't cut it
 # 
 pacstrap -i /mnt base base-devel wireless_tools networkmanager \
     xf86-video-intel vim wget 
@@ -32,49 +32,49 @@ pacstrap -i /mnt base base-devel wireless_tools networkmanager \
 # xorg-server     : xorg server... yeah
 # ttf-inconsolata : nice font
 # rsync           : backup util
-# openvpn		  : vpn all the things
-# openresolv	  : resolve all the dns'
-# git			  : git tools for duplicating / diff tracking source
-# unzip			  : compression utils for zip
-# unrar			  : compression utils for rar
-# p7zip			  : compression utils for 7z/others
-# ntfs-3g		  : file system handler for ntfs
-# exfat-utils	  : file system handler for exfat
-# dosfstools	  : file system handler for fat16/32
-# ranger		  : cli file navigator
-# w3m			  : cli web browser
-# htop			  : cli resource manager
-# feh			  : minimal image viewer
-# ffmpeg		  : video/audio swiss army knife
-# mpv			  : minimal video player
-# calcurse		  : cli calendar / curses based
-# newsboat		  : cli rss reader
-# pulseaudio	  : pulse audio drivers / server
+# openvpn         : vpn all the things
+# openresolv      : resolve all the dns'
+# git             : git tools for duplicating / diff tracking source
+# unzip           : compression utils for zip
+# unrar           : compression utils for rar
+# p7zip           : compression utils for 7z/others
+# ntfs-3g         : file system handler for ntfs
+# exfat-utils     : file system handler for exfat
+# dosfstools      : file system handler for fat16/32
+# ranger          : cli file navigator
+# w3m             : cli web browser
+# htop            : cli resource manager
+# feh             : minimal image viewer
+# ffmpeg          : video/audio swiss army knife
+# mpv             : minimal video player
+# calcurse        : cli calendar / curses based
+# newsboat        : cli rss reader
+# pulseaudio      : pulse audio drivers / server
 # pulseaudio-alsa : pulse - alsa system
-# arandr		  : display handler
-# pavucontrol	  : gui for pulse
-# pamixer		  : pulse mixer
-# mediainfo		  : media meta data reader
-# poppler		  : pdf render library
-# highlight		  : fast and flexible source code highlighter
-# mpd			  : server-side application for playing music
-# mpc			  : cli interface for mpd
-# imagemagick	  : image manipulation program, screenshots through import
-# atool			  : compression handler
-# libcaca		  : cli visualizer library
-# compton		  : compositing suite for wm
-# transset-df	  : transparency handler
-# mupdf			  : pdf / xps viewer
-# evince		  : document viewer
-# youtube-dl	  : cli youtube downloader / can be used for many sites
+# arandr          : display handler
+# pavucontrol     : gui for pulse
+# pamixer         : pulse mixer
+# mediainfo       : media meta data reader
+# poppler         : pdf render library
+# highlight       : fast and flexible source code highlighter
+# mpd             : server-side application for playing music
+# mpc             : cli interface for mpd
+# imagemagick     : image manipulation program, screenshots through import
+# atool           : compression handler
+# libcaca         : cli visualizer library
+# compton         : compositing suite for wm
+# transset-df     : transparency handler
+# mupdf           : pdf / xps viewer
+# evince          : document viewer
+# youtube-dl      : cli youtube downloader / can be used for many sites
 # youtube-viewer  : viewer util
-# pandoc		  : document converter (very haskell heavy)
-# python-dbus	  : python dbus handler (forces python3 install)
+# pandoc          : document converter (very haskell heavy)
+# python-dbus     : python dbus handler (forces python3 install)
 # python-gobject  : python gtk handler
-# blender		  : 3d swiss army knife
-# gimp			  : basically close enough to photoshop
+# blender         : 3d swiss army knife
+# gimp            : basically close enough to photoshop
 # transmission-cli: cli transmission... yeah 
-# screenfetch	  : little os cli printout
+# screenfetch     : little os cli printout
 #
 pacman --needed -Sy xorg-xinit xorg-server ttf-inconsolata rsync openvpn \
     openresolv git unzip unrar p7zip ntfs-3g exfat-utils dosfstools ranger \
@@ -90,7 +90,7 @@ pacman --needed -Sy xorg-xinit xorg-server ttf-inconsolata rsync openvpn \
 # aurget is my alteration, because sometimees you're going to want to alter
 # the config.h before compiling.
 #         + don't forget to updpkgsums after changing anything. other wise,
-#			makepkg will complain.
+#           makepkg will complain.
 #
 aurinstall() { curl -O https://aur.archlinux.org/cgit/aur.git/snapshot/$1.tar.gz \
     && tar -xvf $1.tar.gz && cd $1 && makepkg --noconfirm -si && cd .. \
