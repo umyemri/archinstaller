f671565a8a5633d856e2141b00125db469b9f7f1
umyemri <40582475+umyemri@users.noreply.github.com>
2019-07-02 13:56:16 -0700

    Update install-simple.sh

--- install-simple.sh
+++ install-simple.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#
 # arch installer script
 # by andrew
 # 
@@ -14,27 +15,26 @@
 echo "yes-hello-hi! can i get some details from you?"
 echo -n "    user name: "
 read username
-echo -n "    computer name: "
+echo -ne "\n    computer name: "
 read hostname
-echo -n "    swap size (GiB): "
+echo -ne "\n    swap size (GiB): "
 read swap_size
 echo "okay, $username! i'll get $hostname setup for you with a swap size of $swap_sizeGiB."
-echo -n "    may i begin (y/n)? "
+echo -ne "\n    may i begin (y/n)? "
 read continue
 if [ $continue == 'n' ]; then
 	echo 'oh! okay, exiting. bye!'
 	exit
 fi
 
-echo "\'kay."
-echo "partitioning drives..."
+echo -e "\'kay.\npartitioning drives...\n"
 sleep 1
 
 # first drive: boot partition
 sgdisk -og /dev/sda # -og will clears partition table and sets it to gpt
-sgdisk -n 1:2048:+200MiB -t 1:ef00 /dev/sda # assumes we'll be using a uefi
+sgdisk -n 1:2048:+500MiB -t 1:ef00 /dev/sda # assumes we'll be using a uefi
 
-# make swap space
+# make swap 
 start_of=$(sgdisk -f /dev/sda)
 sgdisk -n 2:$start_of:+$swap_sizeGiB -t 2:8200 /dev/sda
 
@@ -42,18 +42,21 @@ sgdisk -n 2:$start_of:+$swap_sizeGiB -t 2:8200 /dev/sda
 start_of=$(sgdisk -f /dev/sda)
 end_of=$(sgdisk -E /dev/sda)
 sgdisk -n 3:$start_of:$end_of -t 3:8300 /dev/sda
-sgdisk -p /dev/sda
 
 # make home
 sgdisk -og /dev/sdb
 start_of=$(sgdisk -f /dev/sdb)
 end_of=$(sgdisk -E /dev/sdb)
 sgdisk -n 1:$start_of:$end_of -t 1:8300 /dev/sdb
+
+# overview
+sgdisk -p /dev/sda
 sgdisk -p /dev/sdb
+sleep 2
 
 # part 2: formatting drives
 #===================================================================================================
-echo "formatting drives..."
+echo -e "\nformatting drives...\n"
 sleep 1
 mkfs.fat -F32 /dev/sda1
 mkfs.ext4 /dev/sda3
@@ -61,7 +64,7 @@ mkfs.ext4 /dev/sdb1
 mkswap /dev/sda2
 swapon /dev/sda2
 
-echo "mounting drives..."
+echo -e "\nmounting drives...\n"
 sleep 1
 mount /dev/sda3 /mnt
 mkdir /mnt/boot
@@ -71,8 +74,9 @@ mount /dev/sdb1 /mnt/home
 
 # part 3: base install
 #===================================================================================================
-echo "installing base system..."
+echo -e "\ninstalling base system...\n"
 sleep 1
+# i like wireguard. maybe you don't. /shrug
 pacstrap -i /mnt base base-devel nvidia linux-headers vim wget wireguard-dkms wireguard-tools
 
 # part 4: system config
@@ -80,7 +84,7 @@ pacstrap -i /mnt base base-devel nvidia linux-headers vim wget wireguard-dkms wi
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
 
-echo "editing & making things..."
+echo -e "\nediting & making things...\n"
 ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
 hwclock --systohc --utc
 echo "en_US.UTF-8 UTF-8  " >> /etc/locale.gen
@@ -88,12 +92,11 @@ echo "ja_JP.UTF-8 UTF-8  " >> /etc/locale.gen
 locale-gen
 mkinitcpio -p linux
 
-echo "installing bootctl..."
+echo -e "\ninstalling bootctl...\n"
 bootctl --path=/boot install 
 echo "default arch" > /boot/loader/loader.conf
 echo "timeout 0" >> /boot/loader/loader.conf
 echo "editor 0" >> /boot/loader/loader.conf
-
 datUUID=$(blkid | sed -n '/sda3/s/.*UUID=\"\([^\"]*\)\".*/\1/p')
 touch /boot/loader/entries/arch.conf
 echo "title Arch" >> /boot/loader/entries/arch.conf
@@ -104,6 +107,9 @@ echo "options device=UUID=$datUUID root=/dev/sda3 quiet rw" >> /boot/loader/entr
 # add user
 echo hostname >> /etc/hostname
 useradd -m -g wheel -s /bin/bash $username
-echo "done..."
+passwd $username
+
+# exodus
+echo "done!"
+echo "You should run visudo, setup wheel and invalidate root\'s password."
 echo "bye. ∠(ᐛ 」∠)＿"
-exit
