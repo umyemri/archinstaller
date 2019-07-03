2fdfe166ac0049ad3fa2807b077701ff0f72b7ee
umyemri <40582475+umyemri@users.noreply.github.com>
2019-07-02 18:03:29 -0700

    Update payload-simple.sh

--- payload-simple.sh
+++ payload-simple.sh
@@ -2,7 +2,7 @@
 # payload: lots of tools to install
 #
 
-packs = "unzip unrar p7zip ntfs-3g dosfstools htop xorg-xinit xorg-server i3 
+packs = "unzip unrar p7zip ntfs-3g dosfstools htop xorg-xinit xorg-server i3-gaps 
 rxvt-unicode noto-fonts noto-fonts-cjk noto-fonts-emoji feh ffmpeg arandr pulseaudio
 pamixer mpv ranger mediainfo poppler highlight calcurse newsboat cmus atool compton
 transset-df markdown mupdf evince youtube-dl screenfetch uim anthy blender krita"
