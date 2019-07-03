73281d4c959bf47456b017dc2b519d9f1961a164
umyemri <40582475+umyemri@users.noreply.github.com>
2019-07-02 18:01:21 -0700

    Update payload-i3gaps.sh

--- payload-i3gaps.sh
+++ payload-i3gaps.sh
@@ -1,26 +1,10 @@
 #!/bin/bash
+# payload: lots of tools to install
+#
 
-# ricing
-# getting all that sweet ui shiz. Lot of this is based on @LukeSmithXYZ's LARBS - https://github.com/LukeSmithxyz/LARBS
+packs = "unzip unrar p7zip ntfs-3g dosfstools htop xorg-xinit xorg-server i3 
+rxvt-unicode noto-fonts noto-fonts-cjk noto-fonts-emoji feh ffmpeg arandr pulseaudio
+pamixer mpv ranger mediainfo poppler highlight calcurse newsboat cmus atool compton
+transset-df markdown mupdf evince youtube-dl screenfetch uim anthy blender krita"
 
-# don't look at this! I'm just dumping this in.
-pacman --noconfirm --needed -Sy 
-
-# Essentials...
-essent="base-devel sudo wireless_tools network-manager-applet networkmanager rsync git unzip unrar ntfs-3g wget tmux vim w3m dosfstools htop"
-
-# Nice to have...
-nice="xorg-xinit xorg-server rxvt-unicode xf86-video-intel noto-fonts feh ffmpeg pulseaudio pulseaudio-alsa arandr pavucontrol pamixer mpv rofi ranger mediainfo poppler highlight calcurse newsboat mpd mpc ncmpcpp imagemagick atool libcaca compton transset-df markdown mupdf evince youtube-dl youtube-viewer cups scrot offlineimap msmtp notmuch notmuch-mutt r pandoc python-dbus python-gobject transmission-cli projectm-pulseaudio cmatrix asciiquarium screenfetch"
-
-# LaTeX...
-latex="texlive-most texlive-lang biber"
-
-# Big apps...
-bigens="blender gimp"
-
-gpg --recv-keys 5FAF0A6EE7371805 #Add the needed gpg key for neomutt
-
-aurinstall() { curl -O https://aur.archlinux.org/cgit/aur.git/snapshot/$1.tar.gz && tar -xvf $1.tar.gz && cd $1 && makepkg --noconfirm -si && cd .. && rm -rf $1 $1.tar.gz ;}
-
-aurcheck packer i3-gaps vim-pathogen tamzen-font-git neomutt unclutter-xfixes-git urxvt-resize-font-git polybar python-pywal xfce-theme-blackbird fzf-git
-sudo pacman -S --noconfirm --needed i3lock
+pin $packs
