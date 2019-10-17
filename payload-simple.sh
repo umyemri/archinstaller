5eb203daebaea8c308b1fa601ff3527a3e031881
umyemri <40582475+umyemri@users.noreply.github.com>
2019-10-17 14:26:36 -0700

    Update payload-simple.sh

--- payload-simple.sh
+++ payload-simple.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
-# payload: lots of tools to install
+# payload: lots of tools to install / some setup
 #
 
-packs = "unzip unrar p7zip ntfs-3g dosfstools htop xorg-xinit xorg-server i3-gaps 
-rxvt-unicode noto-fonts noto-fonts-cjk noto-fonts-emoji feh ffmpeg arandr pulseaudio
-pamixer mpv ranger mediainfo poppler highlight calcurse newsboat cmus atool compton
+# copying my dot files, you might have other priorities at this point
+echo ". ~/.aliases" >> ~/.bashrc
+githubuser = "umyemri"
+site = "https://raw.githubusercontent.com/$githubuser/archinstaller/master/dot/"
+# curl -sL $site.aliases > ~/.aliases
+# curl -sL $site.xinitrc > ~/.xinitrc
+# curl -sL $site.Xresources > ~/.Xresources
+# wget -O ~/.aliases $site.aliases
+# wget -O ~/.xinitrc $site.xinitrc
+# wget -O ~/.Xresources $site.Xresources
+
+mkdir ~/{docs/{stories,articles,books},vids,pics,data,.config/scripts,tools,games}
+
+packs = "unzip unrar p7zip ntfs-3g dosfstools htop xorg-xinit xorg-server i3-gaps \
+rxvt-unicode noto-fonts noto-fonts-cjk noto-fonts-emoji feh ffmpeg arandr pulseaudio \
+pamixer mpv ranger mediainfo poppler highlight calcurse newsboat cmus atool compton \
 transset-df markdown mupdf evince youtube-dl screenfetch uim anthy blender krita"
 
 pin $packs
