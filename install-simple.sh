d1f7e7828e9d27c3e3249cb7697cb42bf92fa85d
umyemri <40582475+umyemri@users.noreply.github.com>
2019-10-18 13:13:54 -0700

    Update install-simple.sh

--- install-simple.sh
+++ install-simple.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-#
-# arch installer script
-# by andrew
+# title: archinstaller - simple version
+# author: andrew
+# aim: a minimal version of my encryption app
+# last working run: [tbd]
+# notes:
+#   - assumes sda will be a /root /swap disk
+#      - sdb /home
+#      - and you have a connection to the internet
 # 
 # part 1: disks
 # part 2: formatting drives
@@ -9,29 +14,31 @@
 # part 4: system config
 #
 
-packages = "base linux linux-firmware nvidia"
+packages="base linux linux-firmware"
 
 # part 1: disks
 #===================================================================================================
-# user inputs: get desired swap size
-echo "yes-hello-hi! can i get some details from you?"
-echo -n "  user name: "
+echo '\(OoO)/ OH!!'
+sleep 1
+echo 'yes-hello-hi! i am kit. can i get some details from you?'
+echo -n '  user name: '
 read username
-echo -n "  computer name: "
+echo -n '  computer name: '
 read hostname
 echo -n "  swap size (GiB): "
 read swap_size
 echo "okay, $username! i'll get $hostname setup for you with a swap size of $swap_sizeGiB."
-echo -n "  may i begin (y/n)? "
+echo -n '  may i begin (y/n)? '
 read continue
 if [ $continue == 'n' ]; then
 	echo 'oh! okay, exiting. bye!'
+	echo '(._. )...'
 	exit
 fi
+echo '(-u-)\ k! on it!'
+sleep 2
 
-echo -e "\'kay.\npartitioning drives...\n"
-sleep 1
-
+echo -e "\npartitioning drives...\n"
 # first drive: boot partition
 sgdisk -og /dev/sda # -og will clears partition table and sets it to gpt
 sgdisk -n 1:2048:+500MiB -t 1:ef00 /dev/sda # assumes we'll be using a uefi
@@ -54,12 +61,11 @@ sgdisk -n 1:$start_of:$end_of -t 1:8300 /dev/sdb
 # overview
 sgdisk -p /dev/sda
 sgdisk -p /dev/sdb
-sleep 2
+
 
 # part 2: formatting drives
 #===================================================================================================
 echo -e "\nformatting drives...\n"
-sleep 1
 mkfs.fat -F32 /dev/sda1
 mkfs.ext4 /dev/sda3
 mkfs.ext4 /dev/sdb1
@@ -67,7 +73,6 @@ mkswap /dev/sda2
 swapon /dev/sda2
 
 echo -e "\nmounting drives...\n"
-sleep 1
 mount /dev/sda3 /mnt
 mkdir /mnt/boot
 mkdir /mnt/home
@@ -77,41 +82,42 @@ mount /dev/sdb1 /mnt/home
 # part 3: base install
 #===================================================================================================
 echo -e "\ninstalling base system...\n"
-sleep 1
-# i like wireguard. maybe you don't. /shrug
 pacstrap -i /mnt $packages
 
 # part 4: system config
 #===================================================================================================
-genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt
-
 echo -e "\nediting & making things...\n"
-ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
-hwclock --systohc --utc
-echo "en_US.UTF-8 UTF-8  " >> /etc/locale.gen
-echo "ja_JP.UTF-8 UTF-8  " >> /etc/locale.gen 
-locale-gen
-mkinitcpio -p linux
+genfstab -U /mnt >> /mnt/etc/fstab
+arch-chroot /mnt ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
+arch-chroot /mnt hwclock --systohc --utc
+# fuck it, i'll just append the locale.gen file...
+echo "en_US.UTF-8 UTF-8  " >> /mnt/etc/locale.gen
+echo "ja_JP.UTF-8 UTF-8  " >> /mnt/etc/locale.gen
+arch-chroot /mnt locale-gen
+echo $hostname >> /mnt/etc/hostname
+# no need mkinitcpio now... crazy
 
+echo "installing bootctl..."
+#===============================================================================
 echo -e "\ninstalling bootctl...\n"
 bootctl --path=/boot install 
 echo "default arch" > /boot/loader/loader.conf
 echo "timeout 0" >> /boot/loader/loader.conf
 echo "editor 0" >> /boot/loader/loader.conf
-datUUID=$(blkid | sed -n '/sda3/s/.*UUID=\"\([^\"]*\)\".*/\1/p')
+datUUID=$(blkid | sed -n '/sda3/s/.* UUID=\"\([^\"]*\)\".*/\1/p')
 touch /boot/loader/entries/arch.conf
 echo "title Arch" >> /boot/loader/entries/arch.conf
 echo "linux /vmlinuz-linux" >> /boot/loader/entries/arch.conf
 echo "initrd /initramfs-linux.img" >> /boot/loader/entries/arch.conf
 echo "options device=UUID=$datUUID root=/dev/sda3 quiet rw" >> /boot/loader/entries/arch.conf
 
-# add user
-echo hostname >> /etc/hostname
-useradd -m -g wheel -s /bin/bash $username
-passwd $username
+#===============================================================================
+arch-chroot /mnt useradd -m -g wheel -s /bin/bash $username
+arch-chroot /mnt passwd $username
+echo "done..."
+echo "you should do visudo and also disable root\' password."
+echo "bye. (^o^)//"
 
-# exodus
-echo "done!"
-echo -e "You should run visudo, setup wheel and invalidate root\'s password."
-echo "bye. ∠(ᐛ 」∠)＿"
+# seriously, use arch-chroot /mnt visudo
+# otherwise strange things are needed to update things.
+# strange... things... (=_= )
